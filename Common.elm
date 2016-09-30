951904bb682ac414206937cf2c80ecfac788a655
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-09-30 17:15:33 +1000

    Start adding interactive examples

---
+++ Common.elm
@@ -0,0 +1,264 @@
+module Common
+    exposing
+        ( Color(..)
+        , scene2d
+        , point2d
+        , centerPoint2d
+        , direction2d
+        , frame2d
+        , lineSegment2d
+        , triangle2d
+        )
+
+import Html exposing (Html)
+import Svg exposing (Svg)
+import Svg.Attributes as Attributes
+import OpenSolid.Core.Types exposing (..)
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Direction2d as Direction2d
+import OpenSolid.Point2d as Point2d
+import OpenSolid.Frame2d as Frame2d
+import OpenSolid.BoundingBox2d as BoundingBox2d
+import OpenSolid.LineSegment2d as LineSegment2d
+import OpenSolid.Triangle2d as Triangle2d
+import OpenSolid.Svg as Svg
+
+
+config =
+    { scale = 75
+    , stroke =
+        { width = 0.02
+        , thinWidth = 0.01
+        }
+    , point =
+        { radius = 0.05
+        }
+    , originPoint =
+        { radius = 0.05
+        }
+    , centerPoint =
+        { radius = 0.03
+        , crossRadius = 0.1
+        }
+    , direction =
+        { length = 1
+        , tipLength = 0.15
+        , tipWidth = 0.1
+        }
+    }
+
+
+type Color
+    = Black
+    | Blue
+    | Orange
+    | Teal
+
+
+colorString : Color -> String
+colorString color =
+    case color of
+        Black ->
+            "black"
+
+        Blue ->
+            "rgb(0, 109, 219)"
+
+        Orange ->
+            "rgb(219, 109, 0)"
+
+        Teal ->
+            "rgb(0, 146, 146)"
+
+
+scene2d : BoundingBox2d -> List (Svg msg) -> Html msg
+scene2d boundingBox elements =
+    let
+        minX =
+            BoundingBox2d.minX boundingBox
+
+        maxX =
+            BoundingBox2d.maxX boundingBox
+
+        minY =
+            BoundingBox2d.minY boundingBox
+
+        maxY =
+            BoundingBox2d.maxY boundingBox
+
+        width =
+            toString (config.scale * (maxX - minX))
+
+        height =
+            toString (config.scale * (maxY - minY))
+
+        topLeftFrame =
+            Frame2d
+                { originPoint = Point2d ( minX, maxY )
+                , xDirection = Direction2d.x
+                , yDirection = Direction2d.negate Direction2d.y
+                }
+
+        globalAttributes =
+            [ Attributes.strokeWidth (toString config.stroke.width) ]
+    in
+        Svg.svg [ Attributes.width width, Attributes.height height ]
+            [ Svg.g globalAttributes elements
+                |> Svg.relativeTo topLeftFrame
+                |> Svg.scaleAbout Point2d.origin config.scale
+            ]
+
+
+point2d : Color -> Point2d -> Svg msg
+point2d color point =
+    let
+        attributes =
+            [ Attributes.fill (colorString color)
+            , Attributes.r (toString config.point.radius)
+            ]
+    in
+        Svg.point2d attributes point
+
+
+centerPoint2d : Color -> Point2d -> Svg msg
+centerPoint2d color point =
+    let
+        horizontalLine =
+            LineSegment2d
+                ( Point2d ( -config.centerPoint.crossRadius, 0 )
+                , Point2d ( config.centerPoint.crossRadius, 0 )
+                )
+
+        verticalLine =
+            LineSegment2d
+                ( Point2d ( 0, -config.centerPoint.crossRadius )
+                , Point2d ( 0, config.centerPoint.crossRadius )
+                )
+
+        pointAttributes =
+            [ Attributes.fill (colorString color)
+            , Attributes.r (toString config.centerPoint.radius)
+            ]
+
+        lineAttributes =
+            [ Attributes.stroke (colorString color)
+            , Attributes.strokeWidth (toString config.stroke.thinWidth)
+            ]
+    in
+        Svg.g []
+            [ Svg.point2d pointAttributes Point2d.origin
+            , Svg.lineSegment2d lineAttributes verticalLine
+            , Svg.lineSegment2d lineAttributes horizontalLine
+            ]
+            |> Svg.placeIn (Frame2d.at point)
+
+
+originPoint2d : Color -> Point2d -> Svg msg
+originPoint2d color point =
+    let
+        attributes =
+            [ Attributes.fill "white"
+            , Attributes.stroke (colorString color)
+            , Attributes.r (toString config.originPoint.radius)
+            , Attributes.strokeWidth (toString config.stroke.thinWidth)
+            ]
+    in
+        Svg.point2d attributes point
+
+
+direction2d : Color -> Point2d -> Direction2d -> Svg msg
+direction2d color basePoint direction =
+    let
+        frame =
+            Frame2d
+                { originPoint = basePoint
+                , xDirection = direction
+                , yDirection = Direction2d.perpendicularTo direction
+                }
+
+        tipPoint =
+            Point2d ( config.direction.length, 0 )
+
+        stemLength =
+            config.direction.length - config.direction.tipLength
+
+        stemPoint =
+            Point2d ( stemLength, 0 )
+
+        leftPoint =
+            Point2d ( stemLength, config.direction.tipWidth / 2 )
+
+        rightPoint =
+            Point2d ( stemLength, -config.direction.tipWidth / 2 )
+
+        stem =
+            LineSegment2d ( basePoint, stemPoint )
+
+        tip =
+            Triangle2d ( tipPoint, leftPoint, rightPoint )
+
+        attributes =
+            [ Attributes.stroke (colorString color)
+            , Attributes.fill "white"
+            , Attributes.strokeWidth (toString config.stroke.thinWidth)
+            ]
+    in
+        Svg.g attributes [ Svg.lineSegment2d [] stem, Svg.triangle2d [] tip ]
+            |> Svg.placeIn frame
+
+
+frame2d : Color -> Frame2d -> Svg msg
+frame2d color frame =
+    let
+        originPoint =
+            Frame2d.originPoint frame
+
+        xDirection =
+            Frame2d.xDirection frame
+
+        yDirection =
+            Frame2d.yDirection frame
+    in
+        Svg.g []
+            [ direction2d color originPoint xDirection
+            , direction2d color originPoint yDirection
+            , originPoint2d color originPoint
+            ]
+
+
+lineSegment2d : Color -> LineSegment2d -> Svg msg
+lineSegment2d color lineSegment =
+    let
+        ( p1, p2 ) =
+            LineSegment2d.endpoints lineSegment
+
+        lineSegmentAttributes =
+            [ Attributes.stroke (colorString color)
+            , Attributes.strokeWidth (toString config.stroke.width)
+            ]
+    in
+        Svg.g []
+            [ Svg.lineSegment2d lineSegmentAttributes lineSegment
+            , point2d color p1
+            , point2d color p2
+            ]
+
+
+triangle2d : Color -> Triangle2d -> Svg msg
+triangle2d color triangle =
+    let
+        ( p1, p2, p3 ) =
+            Triangle2d.vertices triangle
+
+        triangleAttributes =
+            [ Attributes.stroke (colorString color)
+            , Attributes.strokeWidth (toString config.stroke.width)
+            , Attributes.fill "none"
+            ]
+    in
+        Svg.g []
+            [ Svg.triangle2d triangleAttributes triangle
+            , point2d color p1
+            , point2d color p2
+            , point2d color p3
+            ]
---
+++ Transformations/SimpleRotation.elm
@@ -0,0 +1,64 @@
+module SimpleRotation exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes as Html
+import Html.App as Html
+import Common exposing (..)
+import OpenSolid.Core.Types exposing (..)
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Frame2d as Frame2d
+import OpenSolid.Triangle2d as Triangle2d
+import OpenSolid.Point2d as Point2d
+import Kintail.InputWidget as InputWidget
+
+
+view : Float -> Html Float
+view angle =
+    let
+        boundingBox =
+            BoundingBox2d
+                { minX = -0.5
+                , minY = -0.5
+                , maxX = 2.5
+                , maxY = 3.5
+                }
+
+        triangle =
+            Triangle2d
+                ( Point2d ( 1, 0.5 )
+                , Point2d ( 2, 0.5 )
+                , Point2d ( 2, 2 )
+                )
+
+        rotatedTriangle =
+            Triangle2d.rotateAround Point2d.origin (degrees angle) triangle
+
+        scene =
+            scene2d boundingBox
+                [ frame2d Black Frame2d.xy
+                , triangle2d Blue triangle
+                , triangle2d Orange rotatedTriangle
+                ]
+
+        label =
+            Html.text "Angle:"
+
+        slider =
+            InputWidget.slider [ Html.style [ ( "flex-grow", "1" ) ] ] { min = 0, max = 45, step = 1 } angle
+    in
+        Html.div [ Html.style [ ( "display", "flex" ), ( "flex-direction", "column" ), ( "width", "250px" ), ( "align-items", "center" ) ] ]
+            [ scene
+            , Html.div [ Html.style [ ( "display", "flex" ), ( "flex-direction", "row" ), ( "align-self", "stretch" ) ] ]
+                [ Html.div [ Html.style [ ( "height", "24px" ), ( "line-height", "24px" ), ( "vertical-align", "middle" ) ] ] [ label ]
+                , Html.div [ Html.style [ ( "height", "24px" ), ( "flex-grow", "1" ), ( "display", "flex" ) ] ] [ slider ]
+                ]
+            ]
+
+
+main : Program Never
+main =
+    Html.beginnerProgram
+        { model = 15
+        , view = view
+        , update = always
+        }
